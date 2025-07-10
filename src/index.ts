783d2f94141271bd38a47180820a09e774584fa7
Kevin <kevin.clift.b@gmail.com>
2025-07-09 23:25:18 -0400

    fix bug

--- src/index.ts
+++ src/index.ts
@@ -40,17 +40,16 @@ const proxyMiddleware = createProxyMiddleware<Request, Response>({
   on: {
     proxyReq(proxyReq, req, res) {
       // New request incoming to the proxy
-      if (req.headers["x-event-key"]) {
-        logger.debug(`X-Event-Key: ${req.headers["x-event-key"]}`);
-      }
-      logger.debug(`Received request: ${req.method} ${req.url}`);
+      logger.debug(
+        `Received request: ${req.method} ${req.url} ${req.headers["x-event-key"]}`
+      );
       if (
         req.method == "POST" &&
         req.body &&
         (req.headers["x-event-key"] === "pullrequest:created" ||
           req.headers["x-event-key"] === "pullrequest:updated")
       ) {
-        let bodyWebhookBitbicketCloud = req.body as WebhookBitbucketCloudPR;
+        let bodyWebhookBitbicketCloud = req.body;
         try {
           let bodyData = JSON.stringify(bodyWebhookBitbicketCloud);
           // Set the new headers and body for the proxy request
